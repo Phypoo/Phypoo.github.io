9aa29d9ffd38684448514bb4a75aa38bbbf8687f
Filip Lajtar <lajtar.filip@gmail.com>
2026-07-01 21:29:15 +0200

    there are lots of packages

---
+++ three.js
@@ -0,0 +1,18 @@
+import * as THREE from 'three';
+// import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+// import { GLTFLoader } from 'thr  ee/addons/loaders/GLTFLoader.js';
+
+// const controls = new OrbitControls( camera, renderer.domElement );
+// const loader = new GLTFLoader();
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderer = new THREE.WebGLRenderer({ 
+    canvas : document.querySelector("#3Dcontainer"),   
+});
+
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+camera.position.setZ(30);
+
+renderer.render( scene, camera );
